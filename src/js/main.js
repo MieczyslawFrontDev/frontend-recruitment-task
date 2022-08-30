5c00d902f973263e6374f6785bf7fecaca588775
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-08-30 23:38:51 +0200

    moved logic of counter to another file counter.js, changed name of function handleBackgroundVisibility

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,17 @@
 const buttonElement = document.querySelector(".js-button");
 const iconElement = document.querySelector(".js-icon");
-const resetElement = document.querySelector(".js-resetButton");
 const darkBackgroundElement = document.querySelector(".js-dark");
 
-const handleBackroundVisibility = () => {
+const handleDarkBackround = () => {
   darkBackgroundElement.classList.toggle("js-visible");
 };
 
 const handlePopupVisibility = () => {
   const popupElement = document.querySelector(".js-popup");
   popupElement.classList.toggle("js-visible");
-  handleBackroundVisibility();
+  handleDarkBackround();
 };
 
 buttonElement.addEventListener("click", handlePopupVisibility);
 iconElement.addEventListener("click", handlePopupVisibility);
 darkBackgroundElement.addEventListener("click", handlePopupVisibility);
-
-const handleCounter = () => {
-  const counterElement = document.querySelector(".js-counter");
-  let count = localStorage.getItem("count");
-
-  counterElement.innerText = count++ + " times";
-  if (count > 6) {
-    resetElement.classList.add("js-visible");
-  } else {
-    resetElement.classList.remove("js-visible");
-  }
-  localStorage.setItem("count", count);
-};
-
-buttonElement.addEventListener("click", handleCounter);
-
-const handleCountReset = (count) => {
-  count = localStorage.clear("count");
-  handleCounter();
-};
-
-resetElement.addEventListener("click", handleCountReset);
